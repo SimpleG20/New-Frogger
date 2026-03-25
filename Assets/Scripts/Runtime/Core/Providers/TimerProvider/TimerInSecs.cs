66822b300f8edfc03d30166d3663662fe5038380
Arthur Gonçalves <arthurga54@gmail.com>
2026-03-25 13:43:45 -0300

    ♻️ refactor: restructure TimeProvider to Providers folder

---
+++ Assets/Scripts/Runtime/Core/Providers/TimerProvider/TimerInSecs.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+
+namespace NewFrogger.Core.Data
+{
+    public class TimerInSecs
+    {
+        public event Action<int> OnCountdown;
+
+        public int TimeInSecs { get; private set; }
+        public bool Running { get; private set; }
+
+        private bool _paused;
+        private Action _onEnd;
+
+        public TimerInSecs(int time, Action onEnd = default) 
+        {
+            TimeInSecs = time; 
+            _onEnd = onEnd;
+        }
+
+        public async void Start(CancellationToken ct)
+        {
+            if (Running) return;
+            Running = true;
+
+            try
+            {
+                while (Running && TimeInSecs > 0)
+                {
+                    if (_paused)
+                    {
+                        await UniTask.Yield();
+                        continue;
+                    }
+                    await UniTask.Delay(1000, cancellationToken: ct);
+                    TimeInSecs--;
+                    OnCountdown?.Invoke(TimeInSecs);
+                }
+
+                _onEnd?.Invoke();
+            }
+            finally
+            {
+                Running = false;
+            }
+        }
+        public void Pause() => _paused = true;
+        public void Resume() => _paused = false;
+        public void Stop() => Running = false;
+
+        public void Dispose()
+        {
+            _onEnd = null;
+            OnCountdown = null;
+            
+            Running = false;
+        }
+    }
+}
